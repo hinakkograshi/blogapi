377877b18a5c7573f2b29a96e514438724fca48d
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-01-07 23:25:48 +0900

    improve: handlersパッケージの追加

---
+++ handlers/handlers.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func HelloHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello, world!\n")
+}
+
+func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Posting Article...\n")
+}
+
+func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Article List\n")
+}
+
+func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
+	articleID := 1
+	resString := fmt.Sprintf("Article No.%d\n", articleID)
+	io.WriteString(w, resString)
+}
+
+func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Posting Nice...\n")
+}
+
+func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Posting Comment...\n")
+}
--- main.go
+++ main.go
@@ -1,45 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"log"
 	"net/http"
+
+	"github.com/hinakkograshi/blogapi/handlers"
 )
 
 func main() {
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
-	}
-
-	postArticleHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Posting Article...\n")
-	}
-
-	articleListHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Article List\n")
-	}
-
-	articleDetailHandler := func(w http.ResponseWriter, req *http.Request) {
-		articleID := 1
-		resString := fmt.Sprintf("Article No.%d\n", articleID)
-		io.WriteString(w, resString)
-	}
-
-	postNiceHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Posting Nice...\n")
-	}
-
-	postCommentHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Posting Comment...\n")
-	}
-
-	http.HandleFunc("/hello", helloHandler)
-	http.HandleFunc("/article", postArticleHandler)
-	http.HandleFunc("/article/list", articleListHandler)
-	http.HandleFunc("/article/1", articleDetailHandler)
-	http.HandleFunc("/article/nice", postNiceHandler)
-	http.HandleFunc("/comment", postCommentHandler)
+	http.HandleFunc("/hello", handlers.HelloHandler)
+	http.HandleFunc("/article", handlers.PostArticleHandler)
+	http.HandleFunc("/article/list", handlers.ArticleListHandler)
+	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	http.HandleFunc("/comment", handlers.PostCommentHandler)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
