09cd8f21ffca577a92239ca51aab869590fe755b
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-01-14 21:31:38 +0900

    add: docker-compose.yaml追加 DBセットアップ

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/hinakkograshi/blogapi
 
 go 1.21.5
 
-require github.com/gorilla/mux v1.8.1 
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"database/sql"
+	"fmt"
 
-	"github.com/gorilla/mux"
-	"github.com/hinakkograshi/blogapi/handlers"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	r := mux.NewRouter()
-
-	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
-
-	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
-
-	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
-
-	log.Println("server start at port 8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	dbUser := "docker"
+	dbPassword := "docker"
+	dbDatabase := "sampledb"
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+	if err := db.Ping(); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("connect to DB")
+	}
 }
