1432f1490858360c26f4e7b46ae877db5f4fa5a9
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-01-15 00:50:36 +0900

    add: select文でデータ取得処理

---
+++ insertData.sql
@@ -0,0 +1,12 @@
+# 記事データ2つ
+insert into articles (title, contents, username, nice, created_at) values
+    ('firstPost', 'This is my first blog', 'saki', 2, now());
+insert into articles (title, contents, username, nice) values 
+    ('2nd', 'Second blog post', 'saki', 4);
+
+# コメントデータ 2 つ
+insert into comments (article_id, message, created_at) values
+    (1, '1st comment yeah', now());
+
+insert into comments (article_id, message) values 
+    (1, 'welcome');
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/hinakkograshi/blogapi/models"
 )
 
 func main() {
@@ -17,9 +18,30 @@ func main() {
 		fmt.Println(err)
 	}
 	defer db.Close()
-	if err := db.Ping(); err != nil {
+
+	const sqlStr = `
+    select title, contents, username, nice
+    from articles;
+	`
+	rows, err := db.Query(sqlStr)
+	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println("connect to DB")
+		return
+	}
+	defer rows.Close()
+
+	articleArray := make([]models.Article, 0)
+
+	for rows.Next() {
+		var article models.Article
+		err := rows.Scan(&article.Title, &article.Contents, &article.UserName, &article.NiceNum)
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			articleArray = append(articleArray, article)
+		}
+
+		fmt.Printf("%+v\n", articleArray)
 	}
 }
