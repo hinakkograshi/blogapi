f05def91e7d298cc566c5c96287d4a610fed2653
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-01-17 20:40:26 +0900

    add: repositoriesパッケージの追加

--- main.go
+++ main.go
@@ -1,40 +1,25 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
+	"log"
+	"net/http"
 
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/hinakkograshi/blogapi/models"
+	"github.com/gorilla/mux"
+	"github.com/hinakkograshi/blogapi/handlers"
 )
 
 func main() {
-	dbUser := "docker"
-	dbPassword := "docker"
-	dbDatabase := "sampledb"
-	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+	r := mux.NewRouter()
 
-	db, err := sql.Open("mysql", dbConn)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer db.Close()
+	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
 
-	article := models.Article{
-		Title:    "insert test",
-		Contents: "Can I insert data correctly?",
-		UserName: "hinakko",
-	}
-	const sqlStr = `
-		insert into articles (title, contents, username, nice, created_at) values
-		(?, ?, ?, 0, now());
-	`
-	result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
 
-	fmt.Println(result.LastInsertId())
-	fmt.Println(result.RowsAffected())
+	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+
+	log.Println("server start at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
